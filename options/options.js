1e2a93d9ea83a0a8d3ffa0c49a9284259ecaea1b
Crowdbotics <team@crowdbotics.com>
2024-02-05 19:21:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "digitaltwinsiminfra-45970",
+  url: "https://digitaltwinsiminfra-45970.botics.co",
+  api: "https://digitaltwinsiminfra-45970.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
